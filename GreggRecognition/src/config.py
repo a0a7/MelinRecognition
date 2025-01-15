902373a5f744af846bff7abbb0b519f31909c891
aw <aw@a0.ax>
2025-01-15 09:47:55 -0600

    update conf params

--- GreggRecognition/src/config.py
+++ GreggRecognition/src/config.py
@@ -1,5 +1,9 @@
+import os 
+
 class CONFIG:
     vocabulary_size = 10000
     embedding_size = 256
     RNN_size = 512
     drop_out = 0.5
+    data_folder = os.path.join(os.path.dirname(__file__), 'data')
+    val_proportion = '0.1'
